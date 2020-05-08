9b45590fc81c30a3788c995a8d676f55fd62ce3a
Erim Şengezer <erimsengezer@erim-mbp.local>
2020-05-09 01:43:55 +0300

    feat: wip

---
+++ nBoxApp/Utilities/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  nBoxApp
+//
+//  Created by Erim Şengezer on 8.05.2020.
+//  Copyright © 2020 Erim Şengezer. All rights reserved.
+//
+
+import Foundation
---
+++ nBoxApp/Utilities/NewsModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewsModel.swift
+//  nBoxApp
+//
+//  Created by Erim Şengezer on 9.05.2020.
+//  Copyright © 2020 Erim Şengezer. All rights reserved.
+//
+
+import Foundation
